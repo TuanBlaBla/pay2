a1ada84ed0247ba329bbef16497a6bfc2a16afe5
iamtuann <99541041+iamtuann@users.noreply.github.com>
2022-03-09 14:49:15 +0700

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,35 @@
+const WEB = {
+  title: "My Banking",
+  favicon: "favicon.ico",
+};
+
+const USER = {
+  username: "Nguyễn Huỳnh Thái Tuấn",
+  blueTick: false,
+  avatar: "logonhan.png",
+  skills: [
+      "DỊCH VỤ FACEBOOK",
+      "DEVELOPER"
+  ],
+  banks: [
+      {
+          name: "VPBANK",
+          logo: "vp.png",
+          number: "530062007",
+          owner: "NGUYEN HUYNH THAI TUAN"
+      },
+      {
+          name: "VIETCOMBANK",
+          logo: "vcb.jpg",
+          number: "9383556654",
+          owner: "HUYNH THI HUE LINH"
+      },
+      {
+          name: "MOMO",
+          logo: "momo.jpg",
+          number: "0383556654",
+          owner: "HUYNH THI HUE LINH",
+          fastTransfer: true
+      },
+  ]
+};
